d9df137305adabd38cac42e8db580f6da91597f0
ArthurDourad <111431568+ArthurDourad@users.noreply.github.com>
2022-10-01 15:28:53 -0300

    Add files via upload

---
+++ ADO2/src/Pages/Sobrenos/index.js
@@ -0,0 +1,12 @@
+import { BsHandThumbsUp } from "react-icons/bs";
+function Sobrenos() {
+    return ( 
+        <div>
+            <h1> SOBRE NÓS </h1>
+            <p>
+                Nós somos uma empresa muito querida... Trabalhamos com muuito AMOR e RESPEITO... Não temos PRECONCEITO e NÃO somos XENOFÓBICOS! LULA 13! <BsHandThumbsUp size={15} />
+            </p>
+        </div>
+    )
+}
+export default Sobrenos;
